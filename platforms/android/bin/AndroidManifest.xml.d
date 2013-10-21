d1052048787d9c97893e055d2ebab8b4829b2606
davidrabinowitz <davidrab@gmail.com>
2013-10-21 12:17:21 +0300

    removed generated apps

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/david/dev/workspaces/ecowatcher/nativedroid/platforms/android/bin/AndroidManifest.xml : \
-/home/david/dev/workspaces/ecowatcher/nativedroid/platforms/android/AndroidManifest.xml \
